e6e859bc22dafdf9951812a66e393be118a3f192
Chaitanya Mestry <103654077+Chaitanya2256@users.noreply.github.com>
2025-04-03 04:35:16 +0000

    dev.js

--- 1_basics/basiccode.js
+++ 1_basics/basiccode.js
@@ -1,1 +1,18 @@
 console.log("chaitanya")
+//You have too many running codespaces. You'll need to stop some before you start a new codespace.
+console.log("chaitanya") 
+const accountID = 1200; 
+ //datatype is number 
+ let accHolName = "rahul"  // is string 
+ 
+ var password ="12345"  // is string 
+ let accState;
+  let acc =null; // null is standalone value 
+  //assignment in const cannot be possible again 
+   //var is global scope //let is a block level scope
+  //there is nan not an number //
+  console.log(accountID) 
+console.table({accHolName,accountID,password,accState,acc}) 
+console.log(typeof accState) 
+//type of undefined is that itself console.log(typeof acc); 
+// // type of null is object
