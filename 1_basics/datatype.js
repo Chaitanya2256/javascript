ae2bef051c4ee456ed15fd445b45e262ebd89b2d
Chaitanya Mestry <103654077+Chaitanya2256@users.noreply.github.com>
2025-04-03 07:24:49 +0000

    maths.js

--- 1_basics/datatype.js
+++ 1_basics/datatype.js
@@ -1,5 +1,6 @@
 //primitve null,string ,number ,undefined,symbol
 //ref type  non primitive array objects functions 
 //obj fun
-let hr =["hello",34]
+hr =["hello",34]
 console.log(typeof hr)
+//memory                                                                                                                           
---
+++ 1_basics/nondmaths.js
@@ -0,0 +1,18 @@
+const score =1200;
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(typeof balance);
+
+console.log(balance.toString());  //type of is string
+console.log(balance.toFixed(2));  //type of is string
+let anNo = 111.5677
+console.log(anNo.toPrecision(4))
+
+const hundred = 10000000;
+console.log(hundred.toLocaleString("en-IN"));
+
+//math
+console.log(Math.abs(-3))
