b03cca66c5b990eb6ed73d72e903c74afa8db120
Chaitanya Mestry <103654077+Chaitanya2256@users.noreply.github.com>
2025-04-04 10:00:33 +0000

    fun.js

---
+++ 1_basics/function01.js
@@ -0,0 +1,48 @@
+function twoNo(area){
+    return`the area is ${area}`
+}
+console.log(twoNo(2))
+
+function area(length,breadth){
+len = this.length
+bre = this.breadth
+return
+}
+let key = new area(20,19)
+console.log(key.length);
+
+const user ={
+    username:"chaitu",
+    price:1000
+}
+function handle(anyobj){
+ console.log(`user name is ${user.username}`);
+    
+}
+handle(user) //we pass object
+handle({
+    username:"chaitu",
+    price:1000
+})
+handle(["radhe"])
+function house(){
+    const userName = "rahul"
+    function house1(){
+        const id =230
+        console.log(userName);
+    }
+    //console.log(id); throws error
+   house1()
+}
+house()
+
+//asfunc
+let food = function(){
+    console.log("hello");
+    
+}
+food()
+//arrow fuunc
+let desert = (desert1,desert2)=> console.log(`I have ${desert1} after dinner`);
+
+desert("rasgula","gulabjamun")
